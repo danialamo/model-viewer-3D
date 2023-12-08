148fc4834ad84a9d0eef8ad3c088367518b81630
btopro <btopro@outlook.com>
2023-12-08 11:49:44 -0500

    build fix

--- src/mv-3D-searchDisplay.js
+++ src/mv-3D-searchDisplay.js
@@ -25,7 +25,7 @@ export class mvSearchDisplay extends LitElement{
     }
 
     getUpdateResults(){
-      const address = (new URL('/assets/cards.json', import.meta.url).href); 
+      const address = (new URL('../assets/cards.json', import.meta.url).href); 
       fetch(address).then((response) =>{
         if(response.ok){
           return response.json();
