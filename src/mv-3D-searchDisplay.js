b49c3c91414dcbe3d07fe03ff4ea377b29f0946a
Dani Alamo <dca5244@psu.edu>
2024-01-31 23:09:24 -0500

    Push Changes

--- src/mv-3D-searchDisplay.js
+++ src/mv-3D-searchDisplay.js
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from "lit";
 import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.11.2/cdn/components/input/input.js';
+import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.13.1/cdn/components/divider/divider.js';
 import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.11.2/cdn/components/button/button.js';
 import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.11.2/cdn/components/card/card.js';
 import './mv-modal.js'; 
@@ -37,35 +38,69 @@ export class mvSearchDisplay extends LitElement{
     }
 
     static styles = css`
-     #searchBox{
-        width: 1190px;
-        display: inline-flex;
-        padding: 24px;
-        flex-direction: row;
-        align-items: flex-start;
-        gap: 40px;
-        border-radius: 20px;
-        background: #FFF;
-        box-shadow: 0px 8px 24px 0px rgba(0, 0, 0, 0.20);
-        margin-top: 70px; 
+      :host{
+        width: 70%;
       }
-      sl-input{
-        margin-top: 8px;
+      h2{
+        font-family: 'Montserrat', sans-serif;
+        font-family: 'Open Sans', sans-serif;
+        font-size: 48px;
+        text-align: center;
       }
+      #topSection{
+        width: 100%;
+        margin-top: 100px;
+        display: flex; 
+        justify-content: center;
+        align-items: center;
+      }
+      #modelViewer{
+
+      }
+
+  
     `
     
     render(){
-        return html`
-         <div id="searchBox">
-          <h2>3D Model Viewer</h2>
-            <sl-input placeholder="Search 3D Models..." size="large"></sl-input>
-         </div>
-         
-        `
+      return html`
+        <div ID='topSection'></div>
+           <h2>3D Model Viewer</h2>
+           <div ID="searchBar">
+           <sl-input placeholder="Search all 3D models" size="large" pill>
+           <sl-icon name="search" slot="prefix"></sl-icon>
+           </sl-input>
+           <sl-divider ID= "divider" style="--spacing: 2rem; --width: 4px;" ></sl-divider>
+           </div>
+        </div>
+
+        <div ID="modelViewer">
+        ${this.cards.map(card => html`
+            <sl-card class="card-overview">
+              <img
+                slot="image"
+                src="${card.image}"
+                alt="A spaceman in a suit"
+              />
+              <strong>${card.title}</strong><br/><br/>
+              ${card.description}
+              <br />
+              <div slot="footer">
+                <sl-button variant="default" size="medium" @click="${e => this.show(e, card)}">Learn More</sl-button>
+                <sl-rating></sl-rating>
+              </div>
+            </sl-card>
+            `)}
+            <mv-modal 
+            title="${this.activeTitle}" 
+            text="${this.activeAbout}"
+            embed="${this.activeEmbed}"
+            alt="${this.activeAlt}"
+            src="${this.activeSrc}"
+            modelImage="${this.activemodelImage}"
+            poster="${this.activePoster}"></mv-modal>
+        </div>
+      `
     }
-
-
-
 
 
     show(e, card) {
